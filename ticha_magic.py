d4547287e9f1a9b14b832bb3401da56952e2244f
Ian Fisher <ianfisher45@gmail.com>
2017-06-21 15:57:08 -0400

    Remove deprecated ticha_magic code

--- ticha_magic.py
+++ ticha_magic.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-"""Convert TEI-encoded XML into human-readable HTML.
-
-This module contains two separate conversion programs: the new one, which uses mostly XSLT with
-some Python, and the old one (a descendant of the venerable ticha_magic script) which uses pure
-Python. The old one is deprecated and will probably be removed at some point, but we keep it
-around in case the new design proves horribly misguided.
-"""
+"""Convert TEI-encoded XML into human-readable HTML."""
 import sys
 import os
 import io
@@ -14,8 +8,6 @@ import xml.etree.ElementTree as ET
 
 from lxml import etree, sax
 
-
-### THE CURRENT CONVERSION PROGRAM, USING MOSTLY XSLT ###
 
 XSLT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'transform.xslt')
 
@@ -162,221 +154,6 @@ def tag_eq(tag_in_document, tag_to_check):
     return tag_in_document == tag_to_check or tag_in_document.endswith(':' + tag_to_check)
 
 
-
-## THE OLD CONVERSION PROGRAM, IN PURE PYTHON ##
-
-HEADER_TAGS = ('TEI', 'teiHeader', 'fileDesc', 'titleStmt', 'title',
-    'publicationStmt', 'profileDesc', 'encodingDesc', 'sourceDesc',
-    'langUsage', 'language', 'projectDesc', 'editorialDecl', 'correction',
-    'normalization', 'hyphenation', 'segmentation', 'interpretation',
-    'refsDecl', 'cRefPattern', 'charDecl', 'glyph', 'glyphName', 'figure',
-    'graphic', 'add', 'c', 'del', 'ptr', 'ref', 'g'
-)
-
-SPELLCHOICES = ('orig', 'reg')
-ABBRCHOICES = ('abbr', 'expan')
-
-class TEItoHTMLTarget:
-    def __init__(self, spellchoice='orig', abbrchoice='abbr'):
-        # choice of original (<orig>) or regularized (<reg>) spelling
-        self.spellchoice = spellchoice
-        # choice of abbreviation (<abbr>) or expansion (<expan>)
-        self.abbrchoice = abbrchoice
-        # the stack of open tags (each tag is stored as a name-attrib pair)
-        self.tags = []
-        self.trb = ET.TreeBuilder()
-        self.waiting_for = []
-        self.lines = 0
-        self.this_col_lines = 0
-        self.max_col_lines = 0
-        self.in_column = False
-        self.in_header = True
-        self.trb.start('div')
-
-    def start(self, tag, attrib):
-        if self.waiting_for:
-            return
-        if self.in_header:
-            if tag not in HEADER_TAGS:
-                self.in_header = False
-                #self.open_tag('div', {'class':'col-xs-10'})
-                self.open_tag('div', {'class':'printed_text_page'})
-            else:
-                return
-        # dispatch to the proper tag_ function
-        try:
-            x = getattr(self, 'tag_' + tag)
-            x(attrib)
-        except AttributeError:
-            pass
-        # handle choice tags
-        if tag in SPELLCHOICES and tag != self.spellchoice:
-            self.waiting_for.append(tag)
-        elif tag in ABBRCHOICES and tag != self.abbrchoice:
-            self.waiting_for.append(tag)
-
-    def tag_lb(self, attrib):
-        if self.in_column:
-            self.this_col_lines += 1
-        else:
-            self.lines += 1
-        self.open_and_close_tag('br')
-
-    def tag_head(self, attrib):
-        self.open_tag('h4')
-
-    def tag_hi(self, attrib):
-        if attrib.get('rend') == 'italic':
-            self.open_tag('span', {'class':'italic'})
-        else:
-            self.open_tag('span')
-
-    def tag_foreign(self, attrib):
-        # note that it is rend="italics", with an 's'
-        if attrib.get('rend') == 'italics':
-            self.open_tag('span', {'class':'italic'})
-        else:
-            self.open_tag('span')
-
-    def tag_fw(self, attrib):
-        if attrib.get('type') == 'catch' or attrib.get('type') == 'catchword':
-            self.open_tag('div', {'class':'catch'})
-        elif attrib.get('type') == 'sig':
-            self.open_tag('div', {'class':'sig'})
-        else:
-            self.open_tag('div')
-
-    def tag_pb(self, attrib):
-        if attrib.get('type') != 'pdf':
-            self.new_page()
-
-    def tag_div(self, attrib):
-        if attrib.get('rend') == 'center':
-            self.open_tag('div', {'class':'center'})
-        else:
-            self.open_tag('div')
-
-    def tag_p(self, attrib):
-        if attrib.get('rend') == 'center':
-            self.open_tag('p', {'class':'center'})
-        else:
-            self.open_tag('p')
-
-
-
-    def tag_cb(self, attrib):
-        self.in_column = True
-        n_val = attrib.get('n')
-        if n_val == '1':
-            # value of '1' indicates start of column section
-            self.open_tag('div')
-            self.open_tag('div', {'class':'col-xs-6'})
-        elif n_val == '':
-            # empty value indicates end of column section
-            self.in_column = False
-            self.lines += self.max_col_lines
-            self.max_col_lines = 0
-            self.close_tag('div')
-            self.close_tag('div')
-        else:
-            self.max_col_lines = max(self.max_col_lines, 
-                                     self.this_col_lines)
-            self.this_col_lines = 0
-            self.close_tag('div')
-            self.open_tag('div', {'class':'col-xs-6'})
-
-    def end(self, tag):
-        if self.waiting_for and tag == self.waiting_for[-1]:
-            self.waiting_for.pop()
-        if tag == 'head':
-            self.close_tag('h4')
-        elif tag == 'hi':
-            self.close_tag('span')
-        elif tag == 'fw':
-            self.close_tag('div')
-        elif tag == 'div':
-            self.close_tag('div')
-        elif tag == 'p':
-            self.close_tag('p')
-        elif tag == 'foreign':
-            self.close_tag('span')
-
-    def data(self, data):
-        if not self.waiting_for and not self.in_header:
-            self.trb.data(data)
-
-    def open_tag(self, name, attribs={}):
-        self.tags.append((name, attribs))
-        self.trb.start(name, attribs)
-
-    def open_and_close_tag(self, name, attribs={}):
-        self.trb.start(name, attribs)
-        self.trb.end(name)
-
-    def close_tag(self, name):
-        if len(self.tags) > 0:
-            if self.tags[-1][0] == name:
-                self.tags.pop()
-                self.trb.end(name)
-            else:
-                msg  = 'trying to close <{}>, '.format(name)
-                msg += 'but last tag opened was <{}>'.format(self.tags[-1][0])
-                raise SyntaxError(msg)
-        else:
-            raise SyntaxError('got </{0}> before <{0}>'.format(name))
-
-    def new_page(self):
-        self.close_all_tags()
-        self.lines = 0
-        self.reopen_all_tags()
-
-    def close(self):
-        self.close_all_tags()
-        out = self.trb.close()
-        if out is not None:
-            self.as_str = ET.tostring(out, encoding='unicode')
-        else:
-            self.as_str = ''
-        self.trb = ET.TreeBuilder()
-
-    def close_all_tags(self):
-        # close all open tags
-        for tag, _ in reversed(self.tags):
-            self.trb.end(tag)
-
-    def reopen_all_tags(self):
-        # reopen the closed tags for the next page
-        for tag, attribs in self.tags:
-            self.trb.start(tag, attribs)
-
-    def __str__(self):
-        if not hasattr(self, 'as_str'):
-            self.close()
-        return self.as_str
-
-def line_number_div(lines):
-    line_nos = '\n'.join('{}<br/>'.format(i + 1) for i in range(lines))
-    return '<div class="col-xs-2">{}</div>'.format(line_nos)
-
-def magic_xml_to_html_target(data, *, spellchoice, abbrchoice):
-    """Converts the XML string to HTMl and returns the TEItoHTMLTarget object
-       used to parse. This is useful if you need to get at the individual pages
-       of the document.
-    """
-    target = TEItoHTMLTarget(spellchoice, abbrchoice)
-    parser = ET.XMLParser(target=target)
-    parser.feed(data)
-    parser.close()
-    return target
-
-def magic_xml_to_html(data, *, spellchoice, abbrchoice):
-    """Converts the XML string to a corresponding HTML string."""
-    return str(magic_xml_to_html_target(data, spellchoice=spellchoice, abbrchoice=abbrchoice))
-
-
-
-## THE SCRIPT ##
-
 _preview_template = """\
 <!DOCTYPE html>
 <html lang="en">
@@ -411,22 +188,17 @@ if __name__ == '__main__':
     parser.add_argument('infile', help='file to read XML from')
     parser.add_argument('outfile', nargs='?', help='file to write to')
     parser.add_argument('--preview', action='store_true', help='generate an HTML preview')
+    parser.add_argument('--preprocess', action='store_true', help='apply the XSLT without paginating')
     parser.add_argument('--spellchoice', choices=['orig', 'reg-spacing', 'reg-spanish'], 
                                          default='orig')
     parser.add_argument('--abbrchoice', choices=['abbr', 'expan'], default='abbr')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--magic', action='store_true', help='use the Python ticha_magic parser')
-    group.add_argument('--preprocess', action='store_true',help='apply the XSLT without paginating')
     args = parser.parse_args()
     with open(args.infile, 'r', encoding='utf-8') as ifsock:
         data = ifsock.read()
     # decide which transform function to use, and what outfile name to generate
     infile_name = os.path.splitext(args.infile)[0]
     kwargs = {'abbrchoice':args.abbrchoice, 'spellchoice':args.spellchoice, 'textname':infile_name}
-    if args.magic:
-        data = magic_xml_to_html(data, **kwargs)
-        outfile = args.outfile or infile_name + '.html'
-    elif args.preprocess:
+    if args.preprocess:
         data = tostring(preprocess(fromstring(data), **kwargs))
         outfile = args.outfile or infile_name + '_preprocessed.xml'
     else:
