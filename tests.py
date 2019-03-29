7d7c1e430eccab5bd9ad96c70831116abf666f39
Ian Fisher <iafisher@protonmail.com>
2019-03-29 09:17:00 -0400

    Reformat test runner

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 """
 Test the ticha_magic script.
 
-The test runner loads an XML file, converts it to HTML using ticha_magic, and then compares it to
-a stored answer key file.
+The test runner loads an XML file, converts it to HTML using ticha_magic, and then
+compares it to a stored answer key file.
 """
 from ticha_magic import convert_tei_data
 from make_outline import xml_to_outline
@@ -29,15 +29,22 @@ with open(OUTLINE_ANSWER_KEY, 'r') as infile:
 html_data = convert_tei_data(xml_data, XSLT, flex_file=FLEX)
 if html_data != answer_key:
     print('FAILURE!', end=' ')
-    print('Please inspect {} and compare it to the answer key at {}'.format(FAILED, ANSWER_KEY))
+    print(
+        'Please inspect {} and compare it to the answer key at {}'.format(
+            FAILED, ANSWER_KEY
+        )
+    )
     with open(FAILED, 'w') as f:
         f.write(html_data)
 else:
     outline_data = xml_to_outline(xml_data, 'levanto')
     if outline_data != outline_answer_key:
         print('FAILURE!', end=' ')
-        print('Please inspect {} and compare it to the answer key at {}'.format(
-            FAILED_OUTLINE, OUTLINE_ANSWER_KEY))
+        print(
+            'Please inspect {} and compare it to the answer key at {}'.format(
+                FAILED_OUTLINE, OUTLINE_ANSWER_KEY
+            )
+        )
         with open(FAILED_OUTLINE, 'w') as f:
             f.write(outline_data)
     else:
