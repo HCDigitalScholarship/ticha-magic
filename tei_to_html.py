c049e26bfeb2e803f14201b32966e14fe7248c9a
Ian Fisher <iafisher@protonmail.com>
2019-04-26 10:26:13 -0400

    Implement command-line argument parsing in tei_to_html.py

--- tei_to_html.py
+++ tei_to_html.py
@@ -1,2 +1,16 @@
+import argparse
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert a TEI-encoded text to HTML.")
+    parser.add_argument("infile", help="Path to TEI file to convert")
+    parser.add_argument("-o", help="Prefix for output files")
+    parser.add_argument("--xslt", help="Path to XSLT stylesheet")
+    parser.add_argument("--flex", help="Path to FLEx export (either XML or JSON)")
+    parser.add_argument(
+        "--spellchoice", default="orig", choices=["orig", "reg-spanish", "reg-spacing"]
+    )
+    parser.add_argument("--abbrchoice", default="abbr", choices=["abbr", "expan"])
+    args = parser.parse_args()
+
     print("Not implemented yet!")
